3a6d722dc42111b9f994fa58cf947680e66fd47b
Arun Babu <59362586+saint-mantis@users.noreply.github.com>
2025-08-17 02:25:05 +0530

    updates

--- real_estate/real_estate_demo/real_estate_demo/settings.py
+++ real_estate/real_estate_demo/real_estate_demo/settings.py
@@ -85,11 +85,21 @@ WSGI_APPLICATION = "real_estate_demo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'realestate',
+    'USER': 'sa',
+    'PASSWORD': 'admin123',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
 
 
